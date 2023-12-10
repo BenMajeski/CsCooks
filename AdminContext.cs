9981bcc4eca4d4687bdc2361444bb7cd2a812cba
javestalbennett <143032962+javestalbennett@users.noreply.github.com>
2023-12-10 16:49:33 -0600

    Add files via upload

---
+++ AdminContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace CsKitchen.Models
+{
+    public class AdminContext : DbContext
+    {
+        public AdminContext(DbContextOptions<RecipeContext> options) : base(options) { }
+
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recipe>().HasData(
+                new Admin
+                {
+                    userId = 2,
+                    userName = "testAdminr",
+                    userPassword = "testP@ssword",
+                    userEmail = "testemail2@gmail.com"
+                });
+        }
+    }
+}
